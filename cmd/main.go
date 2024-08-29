1d7b1537441c6c4462779cab40154066de15c4df
AlexBashorin <bashorina5@gmail.com>
2024-08-29 09:59:44 +0700

    added descriptions and TODO

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"fmt"
-
 	"ecwid/internal/parse"
+	"fmt"
 )
 
 func main() {
 	filename := "../large_ip_file.txt"
-	uniqueCount, err := parse.CountUniqueIPs(filename)
+	uniqueCount, pathToUniqIPFile, err := parse.CountUniqueIPs(filename)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
 	}
-	fmt.Printf("Number of unique IP addresses: %d\n", uniqueCount)
+	fmt.Printf("Number of unique IP: %d\n", uniqueCount)
+	fmt.Printf("Path to the file with unique IPs: %s\n", pathToUniqIPFile)
 }
--- internal/parse/parse.go
+++ internal/parse/parse.go
@@ -2,14 +2,16 @@ package parse
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
+// Определяем размер битового массива
 const ipv4Size = 1 << 32
 
+// The structure includes: a bitmap, a counter for the number of unique values, and an array of unique values.
+// структура включает: битой массив, счетчик количества уникальных значений и массив уникальных значений
 type IPv4Counter struct {
 	bitArray    []uint64
 	uniqueCount uint32
@@ -22,18 +24,35 @@ func NewIPv4Counter() *IPv4Counter {
 	}
 }
 
+// adding a bit to our bit array
+// добавление бита в нашем битовом массиве
 func (c *IPv4Counter) AddIP(ip string) {
+	// converting to 32-bit arr
 	ipInt := ipToInt(ip)
+	// get index for array and bit
 	index, bit := ipInt/64, ipInt%64
+	// create mask 64-bit and place our bit
 	mask := uint64(1) << bit
 
+	// AND between 64-bit array and mask: if array don't have value in this index, place our bit (with mask)
+	// 10101100 and 00010000 - we will got 0 (1 & 0 = 0)
+	// побитовая операция AND между значением из массива и текущей маской, если нет занчения (0), добавляем
+	// например, 10101100 and 00010000 - получим 0, т.к. 1 & 0 = 0
 	if c.bitArray[index]&mask == 0 {
+		// OR with same: got 1 (1 | 0 = 1) and assigning a value (bit to the desired index)
+		// присваиваем плюс побитовое ИЛИ: если значение установлено - не трогаем, иначе записываем
 		c.bitArray[index] |= mask
 		c.uniqueCount++
+		// append this unique IP
+		// добавляем в массив (тут тудушка: сделать проверку на уникальность по ходу, а не просто записывать в отдельный массив)
 		c.uniq_ip = append(c.uniq_ip, ip)
 	}
+	// TODO:
+	// remove not unique IP in the process, without additional array
 }
 
+// converting a string representation of an IP to a 32-bit number
+// преобразование строкового представления IP в 32 битное число
 func ipToInt(ip string) uint32 {
 	octets := strings.Split(ip, ".")
 	var result uint32
@@ -44,10 +63,10 @@ func ipToInt(ip string) uint32 {
 	return result
 }
 
-func CountUniqueIPs(filename string) (uint32, error) {
+func CountUniqueIPs(filename string) (uint32, string, error) {
 	file, err := os.Open(filename)
 	if err != nil {
-		return 0, err
+		return 0, "", err
 	}
 	defer file.Close()
 
@@ -59,16 +78,16 @@ func CountUniqueIPs(filename string) (uint32, error) {
 	}
 
 	if err := scanner.Err(); err != nil {
-		return 0, err
+		return 0, "", err
 	}
 
-	fmt.Println(counter.uniq_ip)
 	if len(counter.uniq_ip) > 0 {
+		// TODO: Error: open ./unique.txt: too many open files
 		for i := 0; i < len(counter.uniq_ip); i++ {
 			unic_ips, err := os.OpenFile("./unique.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
 			if err != nil {
-				return 0, err
+				return 0, "", err
 			}
 			defer unic_ips.Close()
 
@@ -76,5 +95,5 @@ func CountUniqueIPs(filename string) (uint32, error) {
 		}
 	}
 
-	return counter.uniqueCount, nil
+	return counter.uniqueCount, "./unique.txt", nil
 }
